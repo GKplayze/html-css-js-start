dcbe3ef70d202c57ad9c982a24371c315ba01f25
GKplayze <87566482+GKplayze@users.noreply.github.com>
2022-04-23 12:19:46 +0200

    initial commit Change h1 element with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css";
+
+let txtBtn = document.querySelector(".text-btn");
+
+txtBtn.addEventListener("click", function () {
+  document.getElementById("demo").innerText = "Pow Wow!";
+  console.log("Pow Wow!");
+});
